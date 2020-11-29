e7a84f8fb7338a1c801a57f993212cf1e020c82b
Guest <guest-qymtrf@h26820.sgb.skir.kr>
2020-11-29 13:34:20 +0900

    build template

---
+++ AuthenticodeGenerator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+class CodeGenerator{
+private:
+	char *m_code;
+public:
+	CodeGenerator();
+	CodeGenerator(string ID);	
+};
+
+class PersonalInformation{
+private:
+	string m_ID;
+public:
+	void setID();
+	string getID();
+};
+
+int main()
+{
+	while()
+	return 0;
+}
